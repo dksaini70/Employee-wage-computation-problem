3cf41ea05d7ce9b5d8417a84bacfe881457afef8
dksaini70 <dkreview70@gmail.com>
2021-06-26 18:09:24 +0530

    store day and daily wage

--- employee-wage.sh
+++ employee-wage.sh
@@ -6,6 +6,7 @@ parttime=2
 maxhrs=100
 emp_rate_pr_hr=20
 numtotalworkingday=20
+declare -A dailywage
 
 totalworkinghr=0
 totalwokingday=0
@@ -29,9 +30,13 @@ do
         attend=$((RANDOM%3))
 	getworkinghr $attend
 	totalworkinghr=$(($totalworkinghr + $workinghr ))
-	dailywage[$totalworkingday]=$(($workinghr*$emp_rate_pr_hr))
+	dailywage[ "day""$totalworkingday"]=$(($workinghr*$emp_rate_pr_hr))
+
 done
-totalsalary=$(($totalworkinghr * $emp_rate_pr_hr))
-echo "total salar is $totalsalary"
 echo "daily wage"
 echo  ${dailywage[@]}
+echo "days"
+echo  ${!dailywage[@]}
+
+totalsalary=$(($totalworkinghr * $emp_rate_pr_hr))
+echo "total salar is $totalsalary"
