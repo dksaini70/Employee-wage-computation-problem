5bf9fb4bde6666a7e6b6e8ebbf62ee8666083721
dksaini70 <dkreview70@gmail.com>
2021-06-26 14:11:15 +0530

    add parttime employee-wage

--- employee-wage.sh
+++ employee-wage.sh
@@ -2,12 +2,17 @@
 echo "Welcome to Employee Wage Computation"
 present=1
 absent=0
+parttime=2
 wageprhr=20
-attend=$((RANDOM%2))
+attend=$((RANDOM%3))
 if [ $attend -eq $present ]
 then
 fulldayhr=8
 echo "employee is present"
+elif [ $attend -eq $parttime ]
+then
+echo "employee is parttime"
+fulldayhr=4
 else
 fulldayhr=0
 echo "employee is absent"
